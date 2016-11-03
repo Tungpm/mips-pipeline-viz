badc54bdd9d0b0a30dfb880fe13a1c4af5997af4
Son Pham <skp011@bucknell.edu>
2016-11-02 20:35:29 -0400

    MIPS Translator And Other Stuffs

---
+++ filereader.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Son Pham on 11/2/2016.
+ */
+
+$.get('include/hello.txt', function(myContentFile) {
+
+  console.log(myContentFile);
+
+  // Split files into line
+  var lines = myContentFile.split(/\n/);
+
+  for (var i  in lines) {
+    console.log(lines[i])
+  }
+}, 'text');
